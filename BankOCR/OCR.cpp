0750f2d655ead7d027501bb1e2f5577973d15696
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2017-07-11 22:14:45 +0200

    refactored for more expressiveness

--- BankOCR/OCR.cpp
+++ BankOCR/OCR.cpp
@@ -1,124 +1,296 @@
 #include "OCR.h"
+
 #include <cassert>
+#include <optional>
+#include <algorithm>
+#include <numeric>
+#include <type_traits>
 
-
-std::vector<int> OCR::read(const std::string& input)
-{
-	assert(4 * 27 == input.size());
-	std::vector<int> result;
-	for (int i = 0; i < 9; ++i) {
-		std::string sign = getPos(input, i);
-		result.push_back( getNumber(sign) );
+namespace meta {
+	template<class Container, class Callback>
+	auto transform_to_vector(const Container& input, Callback&& callback) {
+		using std::begin;
+		using std::end;
+		using Input = decltype(*begin(input));
+		using Output = std::result_of_t<Callback(Input)>;
+		std::vector<Output> result;
+		result.resize(input.size());
+		std::transform(begin(input), end(input), result.begin(), (Callback&&)callback);
+		return result;
 	}
-	return result;
-}
 
-namespace {
-	std::string charArray[10] = {
-		" _ "
-		"| |"
-		"|_|",
-		"   "
-		"  |"
-		"  |",
-		" _ "
-		" _|"
-		"|_ ",
-		" _ "
-		" _|"
-		" _|",
-		"   "
-		"|_|"
-		"  |",
-		" _ "
-		"|_ "
-		" _|",
-		" _ "
-		"|_ "
-		"|_|",
-		" _ "
-		"  |"
-		"  |",
-		" _ "
-		"|_|"
-		"|_|",
-		" _ "
-		"|_|"
-		" _|" };
-}
-
-std::string OCR::getPos(const std::string& input, int i) {
-	return 
-		input.substr(i * 3, 3)
-		+ input.substr(27 + i* 3, 3) 
-		+ input.substr(2*27 + i * 3, 3);
-}
-
-int OCR::getNumber(const std::string& str) {
-	auto i = 0;
-	for (auto ch : charArray) {
-		if (ch == str) return i;
-		i++;
+	template<class Container, class Callback>
+	auto transform_to_string(const Container& input, Callback&& callback) {
+		using std::begin;
+		using std::end;
+		std::string result;
+		result.resize(input.size());
+		std::transform(begin(input), end(input), result.begin(), (Callback&&)callback);
+		return result;
 	}
-	return -1;
-}
 
-
-int getCheckSum(const std::vector<int>& in) {
-	auto ret = 0, p = 0;
-	for (auto v : in) ret += (9 - p++) * v;
-	return ret % 11;
-}
-
-std::string getCheck(const std::vector<int>& in)
-{
-	std::string ret = "";
-	for (int i : in) ret += i < 0 ? '?' : '0'+i;
-
-	for (auto n : in) {
-		if (n < 0) return ret + " ILL";
+	template<class Container, class Callback>
+	auto transform_to_array(const Container& input, Callback&& callback) {
+		using std::begin;
+		using std::end;
+		using Input = decltype(*begin(input));
+		using Output = std::result_of_t<Callback(Input)>;
+		std::array<Output, Container().size()> result;
+		std::transform(begin(input), end(input), result.begin(), (Callback&&)callback);
+		return result;
 	}
-	return ret + (getCheckSum(in) ? " ERR" : "");
+
+	template<class Container, class Target, class Callback>
+	auto accumulate(const Container& input, const Target& accu, Callback&& callback) {
+		using std::begin;
+		using std::end;
+		return std::accumulate(begin(input), end(input), accu, (Callback&&)callback);
+	}
+
+	template<class Container, class Callback>
+	auto all_of(const Container& input, Callback&& callback) {
+		using std::begin;
+		using std::end;
+		return std::all_of(begin(input), end(input), (Callback&&)callback);
+	}
+
+	template<class Container, class Value>
+	auto find_value_index(const Container& container, const Value& value) {
+		using std::begin;
+		using std::end;
+		using optional_index = std::optional<size_t>;
+		auto it = std::find(begin(container), end(container), value);
+		if (it == end(container)) return optional_index();
+		auto index = std::distance(begin(container), it);
+		return optional_index(index);
+	}
+
+	template<size_t count, class Container>
+	auto& part(Container& container, size_t index) {
+		using std::begin;
+		using Element = std::remove_reference_t<decltype(*begin(container))>;
+		using Array = std::array<std::remove_const_t<Element>, count>;
+		using ConstFixed = std::conditional_t<std::is_const_v<Element>, const Array*, Array*>;
+		return *reinterpret_cast<ConstFixed>(&container[index*count]);
+	}
+
+	template<class E, size_t S>
+	constexpr auto str_array(E(&v)[S]) {
+		std::array<std::remove_const_t<E>, S - 1> result;
+		for (auto i = 0u; i < S - 1; ++i) result[i] = v[i];
+		return result;
+	}
+
+	template<class E, class... A>
+	constexpr auto mk_array(E e, A... a) {
+		std::array<std::remove_const_t<E>, 1 + sizeof...(A)> result = { { e, a... } };
+		return result;
+	}
 }
 
-std::vector<std::vector<int>> replacements = { { 8 },{ 7 },{},{ 9 },{},{ 6,9 },{},{ 1 },{ 0,6,9 },{ 3,5,8 } };
+namespace BankOCR {
+	using ScannedDigit = std::array<char, 9>;
+	using ScannedDigitList = ScannedDigit[10];
+	namespace {
+		const ScannedDigitList scannedDigitList = {
+			meta::str_array(
+				" _ "
+				"| |"
+				"|_|"),
+			meta::str_array(
+				"   "
+				"  |"
+				"  |"),
+			meta::str_array(
+				" _ "
+				" _|"
+				"|_ "),
+			meta::str_array(
+				" _ "
+				" _|"
+				" _|"),
+			meta::str_array(
+				"   "
+				"|_|"
+				"  |"),
+			meta::str_array(
+				" _ "
+				"|_ "
+				" _|"),
+			meta::str_array(
+				" _ "
+				"|_ "
+				"|_|"),
+			meta::str_array(
+				" _ "
+				"  |"
+				"  |"),
+			meta::str_array(
+				" _ "
+				"|_|"
+				"|_|"),
+			meta::str_array(
+				" _ "
+				"|_|"
+				" _|")
+		};
 
-std::vector<std::vector<int>> checkReplace(std::vector<int> in)
-{
-	std::vector<std::vector<int>> results;
-	for (auto &i : in) {
-		int org = i;
-		for (auto r : replacements[i])
-		{
-			i = r;
-			if (getCheckSum(in) == 0 ) {
-				results.push_back( in );
-				if (results.size() > 1) {
-					return results;
-				}
-			};
+		AccountDigit parseDigit(const ScannedDigit& scanned) {
+			auto optional_index = meta::find_value_index(scannedDigitList, scanned);
+			if (optional_index) return static_cast<AccountDigit>(*optional_index);
+			return AccountDigit::Invalid;
 		}
-		i = org;
 	}
-	return results;
-}
 
-std::string getCheckPlus(const std::vector<int>& in)
-{
-	std::string ret = "";
-	for (int i : in) ret += i < 0 ? '?' : '0' + i;
-
-	for (auto n : in) {
-		if (n < 0) return ret + " ILL";
+	using ScannedDigitLine = std::array<ScannedDigit, AccountNumber().size()>;
+	namespace {
+		auto chopScannedEntry(const ScannedEntry& entry) {
+			ScannedDigitLine result;
+			for (auto digit = 0u; digit < AccountNumber().size(); ++digit) {
+				for (auto line = 0u; line < 3; ++line)
+					meta::part<3>(result[digit], line) = meta::part<3>(entry[line], digit);
+			}
+			return result;
+		}
 	}
-	if (0 == getCheckSum(in)) return ret;
-	auto v = checkReplace(in);
 
-	if (v.empty()) return ret + " ERR";
-	if (v.size() != 1) return ret + " AMB";
+	AccountNumbers parse(ScannedFile input)
+	{
+		return meta::transform_to_vector(input, [](const auto& entry)->AccountNumber {
+			return meta::transform_to_array(chopScannedEntry(entry), &parseDigit);
+		});
+	}
 
-	ret = "";
-	for (int i : v[0]) ret += '0' + i;
-	return ret + " FIX";
+	namespace {
+		auto isValid(AccountNumber accountNumber) {
+			return meta::all_of(accountNumber, [](auto v) { return v != AccountDigit::Invalid; });
+		}
+
+		auto calcChecksum(AccountNumber accountNumber) {
+			return meta::accumulate(accountNumber, 0, [p = 9](auto accu, auto v) mutable {
+				return accu + (p--) * static_cast<int>(v);
+			}) % 11;
+		}
+	}
+
+	CheckResults check(AccountNumbers accountNumbers)
+	{
+		return meta::transform_to_vector(accountNumbers, [](const auto& accountNumber) {
+			CheckResult result;
+			result.number = accountNumber;
+			if (!isValid(accountNumber)) {
+				result.state = CheckState::IllelibleCharacters;
+			}
+			else if (0 != calcChecksum(accountNumber)) {
+				result.state = CheckState::WrongChecksum;
+			}
+			return result;
+		});
+	}
+
+	using Alternatives = std::vector<AccountDigit>;
+	using AlternativeMap = std::array<Alternatives, 10>;
+	namespace {
+		const AlternativeMap alternativeMap = meta::mk_array(
+			Alternatives{ AccountDigit::Eight }, // 0
+			Alternatives{ AccountDigit::Seven }, // 1
+			Alternatives{},  // 2
+			Alternatives{ AccountDigit::Nine }, // 3
+			Alternatives{}, // 4
+			Alternatives{ AccountDigit::Six, AccountDigit::Nine }, // 5
+			Alternatives{},  // 6
+			Alternatives{ AccountDigit::One }, // 7
+			Alternatives{ AccountDigit::Zero, AccountDigit::Six, AccountDigit::Nine }, // 8
+			Alternatives{ AccountDigit::Three, AccountDigit::Five, AccountDigit::Eight } // 9
+		);
+
+		auto gatherFixes(AccountNumber accountNumber) {
+			AccountNumbers result;
+			for (auto& digit : accountNumber) {
+				auto backup = digit;
+				for (auto alternative : alternativeMap[static_cast<int>(backup)]) {
+					digit = alternative;
+					if (0 == calcChecksum(accountNumber)) result.push_back(accountNumber);
+				}
+				digit = backup;
+			}
+			return result;
+		}
+	}
+
+	CheckResults checkWithFixes(AccountNumbers accountNumbers)
+	{
+		return meta::transform_to_vector(accountNumbers, [](const auto& accountNumber) {
+			CheckResult result;
+			result.number = accountNumber;
+			if (!isValid(accountNumber)) {
+				result.state = CheckState::IllelibleCharacters;
+			}
+			else if (0 != calcChecksum(accountNumber)) {
+				result.state = CheckState::WrongChecksum;
+
+				auto fixes = gatherFixes(accountNumber);
+				if (fixes.empty()) result.fix = FixState::Failed;
+				else if (1 != fixes.size()) result.fix = FixState::Ambiguous;
+				else {
+					result.fix = FixState::Success;
+					result.number = fixes.front();
+				}
+			}
+			return result;
+		});
+	}
+
+	namespace {
+		const std::array<char, 11> accountDigitToChar = meta::str_array("0123456789?");
+	}
+
+	String toString(AccountNumber accountNumber)
+	{
+		return meta::transform_to_string(accountNumber, [](const auto& accountDigit) {
+			return accountDigitToChar[static_cast<size_t>(accountDigit)];
+		});
+	}
+
+	String toString(CheckResults checkResults)
+	{
+		auto lines = meta::transform_to_vector(checkResults, [](const auto& checkResult) {
+			auto numberStr = toString(checkResult.number);
+			switch (checkResult.state) {
+			case CheckState::Passed:
+				return numberStr;
+
+			case CheckState::IllelibleCharacters:
+				return numberStr + " ILL";
+
+			case CheckState::WrongChecksum:
+				switch (checkResult.fix) {
+				case FixState::Success:
+					return numberStr + " FIX";
+				case FixState::Ambiguous:
+					return numberStr + " AMB";
+				default:
+				case FixState::Failed:
+					return numberStr + " ERR";
+				}
+			default:
+				return String{};
+			}
+		});
+		return meta::accumulate(lines, String{}, [](auto accu, auto line) {
+			if (accu.empty()) return line;
+			return accu + "\n" + line;
+		});
+	}
+
+	int to_int(AccountNumber accountNumber)
+	{
+		return meta::accumulate(accountNumber, 0, [](auto accu, auto v) {
+			if (accu >= 0 && v != AccountDigit::Invalid)
+				return accu * 10 + static_cast<int>(v);
+			else
+				return -1;
+		});
+	}
+
 }
-
--- BankOCR/OCR.h
+++ BankOCR/OCR.h
@@ -1,21 +1,48 @@
 #pragma once
+#include <array>
 #include <string>
 #include <vector>
 
-class OCR
+namespace BankOCR
 {
-public:
-	// multiline string => vector of digits
-	static std::vector<int> read(const std::string& input);
+	using String = std::string;
+
+	using ScannedLine = std::array<char, 27>;
+	using ScannedEntry = std::array<ScannedLine, 4>;
+	using ScannedFile = std::vector<ScannedEntry>;
+
+	enum class AccountDigit {
+		Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Invalid
+	};
+	using AccountNumber = std::array<AccountDigit, 9>;
+	using AccountNumbers = std::vector<AccountNumber>;
+
+	enum class CheckState {
+		Passed,
+		WrongChecksum,
+		IllelibleCharacters,
+	};
+	enum class FixState {
+		None,
+		Success,
+		Ambiguous,
+		Failed,
+	};
+	struct CheckResult {
+		CheckState state = CheckState::Passed;
+		FixState fix = FixState::None;
+		AccountNumber number;
+	};
+	using CheckResults = std::vector<CheckResult>;
 
 
-	static std::string getPos(const std::string& input, int i);
-	static int getNumber(const std::string& str);
-};
+	AccountNumbers parse(ScannedFile);
 
-int getCheckSum(const std::vector<int>& in);
+	CheckResults check(AccountNumbers);
+	CheckResults checkWithFixes(AccountNumbers);
 
-std::string getCheck(const std::vector<int>& in);
-std::string getCheckPlus(const std::vector<int>& in);
+	String toString(AccountNumber);
+	String toString(CheckResults);
 
-std::vector<std::vector<int>> checkReplace(std::vector<int> in);
+	int to_int(AccountNumber);
+}
